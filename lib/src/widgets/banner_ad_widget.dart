790dd9b05fdef26e36bdf7ee2db9650273dcc45a
Vivek <vivek.techrizz@gmail.com>
2025-11-29 19:26:37 +0530

    Change Banner ads code

--- lib/src/widgets/banner_ad_widget.dart
+++ lib/src/widgets/banner_ad_widget.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ads_preloader/flutter_ads_preloader.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:provider/provider.dart';
 
 class BannerAdWidget extends StatefulWidget {
-  final double? width;
-  const BannerAdWidget({super.key, this.width});
+  final String adUnitId;
+  const BannerAdWidget({super.key, required this.adUnitId});
 
   @override
   State<BannerAdWidget> createState() => _BannerAdWidgetState();
@@ -13,27 +11,21 @@ class BannerAdWidget extends StatefulWidget {
 
 class _BannerAdWidgetState extends State<BannerAdWidget> {
   BannerAd? _banner;
-  bool _isLoaded = false;
+  bool _loaded = false;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
+  void initState() {
+    super.initState();
 
-    final ads = Provider.of<AdsProvider>(context, listen: false);
-
-    // Create a NEW banner instance for every BannerAdWidget usage
     _banner = BannerAd(
-      adUnitId: ads.bannerAd?.adUnitId ?? "",
+      adUnitId: widget.adUnitId,
       size: AdSize.banner,
       request: const AdRequest(),
       listener: BannerAdListener(
-        onAdLoaded: (_) {
-          setState(() => _isLoaded = true);
-        },
+        onAdLoaded: (_) => setState(() => _loaded = true),
         onAdFailedToLoad: (ad, error) {
-              print("------>>> Error --> $error");
-          setState(() => _isLoaded = false);
           ad.dispose();
+          setState(() => _loaded = false);
         },
       ),
     )..load();
@@ -47,15 +39,10 @@ class _BannerAdWidgetState extends State<BannerAdWidget> {
 
   @override
   Widget build(BuildContext context) {
-    // Return empty widget if banner not loaded - takes up NO space
-    if (_banner == null || !_isLoaded) {
-      return const SizedBox.shrink();
-    }
+    if (!_loaded) return const SizedBox.shrink();
 
-    return Container(
+    return SizedBox(
       height: _banner!.size.height.toDouble(),
-      width: widget.width ?? _banner!.size.width.toDouble(),
-      margin: const EdgeInsets.symmetric(vertical: 4),
       child: AdWidget(ad: _banner!),
     );
   }
