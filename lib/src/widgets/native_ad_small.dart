3972e1fcf02be8ac0aec679493acb4f0f61f0971
Vivek <vivek.techrizz@gmail.com>
2025-12-11 11:30:25 +0530

    NativeAdSmallWidget fix

--- lib/src/widgets/native_ad_small.dart
+++ lib/src/widgets/native_ad_small.dart
@@ -26,6 +26,9 @@ class _NativeAdSmallWidgetState extends State<NativeAdSmallWidget> {
     _nativeAd = NativeAd(
       adUnitId: widget.adUnitId,
       request: const AdRequest(),
+      nativeTemplateStyle: NativeTemplateStyle(
+        templateType: TemplateType.small,
+      ),
       listener: NativeAdListener(
         onAdLoaded: (ad) {
           setState(() => _isLoaded = true);
