0df85cf21c51f0e224b6a8d4300573858038b871
Vivek <vivek.techrizz@gmail.com>
2025-12-12 12:40:49 +0530

    fix

--- lib/src/widgets/native_ad_medium.dart
+++ lib/src/widgets/native_ad_medium.dart
@@ -49,6 +49,6 @@ class _NativeAdMediumWidgetState extends State<NativeAdMediumWidget> {
   Widget build(BuildContext context) {
     if (!_loaded) return SizedBox.shrink();
 
-    return AdWidget(ad: _nativeAd!);
+    return SizedBox(height: 350, child: AdWidget(ad: _nativeAd!));
   }
 }
